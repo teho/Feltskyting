50e1cceec5609af7029586139c90162867f35f6a
Terje Hopsø <terje@hopso.no>
2014-03-25 19:20:54 +0100

    Ryddet kode Legger til data-transition for bedre bytte av sider.

--- js/feltskyting.js
+++ js/feltskyting.js
@@ -19,22 +19,22 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // onDeviceReady - Device is ready for use
 // 	********************************************
 function onDeviceReady() {
-    console.log("onDevice called");
-    try {
-        db = window.openDatabase("FeltDB", "1.0", "Feltskyting", 200000);
-    } catch (err) {
-        alert(err);
-    }
-    // Skal fjernes senere. Skal være brukerstyrt
-    db.transaction(initierDatabase, DbErrorHandler, initierDropDowns);
+	console.log("onDevice called");
+	try {
+		db = window.openDatabase("FeltDB", "1.0", "Feltskyting", 200000);
+	} catch (err) {
+		alert(err);
+	}
+	// Skal fjernes senere. Skal være brukerstyrt
+	db.transaction(initierDatabase, DbErrorHandler, initierDropDowns);
 }
 
 window.onBeforeUnload = function() {
-    console.log("window.onBeforeUnload");
+	console.log("window.onBeforeUnload");
 };
 window.onUnload = function() {
-    console.log("window.onUnload");
-    // navigator.app.exitApp();
+	console.log("window.onUnload");
+	// navigator.app.exitApp();
 };
 
 // document.addEventListener("menubutton", function () {
@@ -51,127 +51,130 @@ window.onUnload = function() {
 
 document.addEventListener("resume", onResume, false);
 function onResume() {
-    console.log("Resume event");
+	console.log("Resume event");
 };
 
 document.addEventListener("pause", onPause, false);
 function onPause() {
-    console.log("Pause event");
+	console.log("Pause event");
 };
 
 //  ********************************************
 //  Parameter endret
 // 	********************************************
 function parameterEndring(obj) {
-    console.log("parameterEndring");
-    console.log(obj);
-    // console.log(obj.options[obj.selectedIndex].value);
-    console.log(obj.options[obj.selectedIndex].innerHTML);
+	console.log("parameterEndring");
+	console.log(obj);
+	// console.log(obj.options[obj.selectedIndex].value);
+	console.log(obj.options[obj.selectedIndex].innerHTML);
 }
 
 //  ********************************************
 //  Oppdater Dropdown fra resultatsett
 // 	********************************************
 function oppdaterListe(tx, res, selId) {
-    console.log("oppdaterListe");
-    var rec, i;
+	console.log("oppdaterListe");
+	var rec, i;
 
-    // Løp gjennom resultatene og fyll opp listen
-    for ( i = 0; i < res.rows.length; i++) {
-        rec = res.rows.item(i);
-        selId.append($("<OPTION />").val(rec.id).text(rec.navn));
-    };
+	// Løp gjennom resultatene og fyll opp listen
+	for ( i = 0; i < res.rows.length; i++) {
+		rec = res.rows.item(i);
+		selId.append($("<OPTION />").val(rec.id).text(rec.navn));
+	};
 
-    // Sett fokus på første element og oppdater skjerm
-    selId.prop('selectedIndex', 0).selectmenu('refresh');
+	// Sett fokus på første element og oppdater skjerm
+	selId.prop('selectedIndex', 0).selectmenu('refresh');
 }
 
 //  ********************************************
 //  Initier Liste
 // 	********************************************
 function initierListe(tx, tbl, selId) {
-    console.log("initierListe: " + tbl);
-    tx.executeSql('SELECT id, navn FROM ' + tbl + " order by navn", [], function(tx, res) {
-        oppdaterListe(tx, res, selId);
-    }, DbErrorHandler);
+	console.log("initierListe: " + tbl);
+	tx.executeSql('SELECT id, navn FROM ' + tbl + " order by navn", [], function(tx, res) {
+		oppdaterListe(tx, res, selId);
+	}, DbErrorHandler);
 }
 
 //  ********************************************
 //  Initier DropDowns
 // 	********************************************
 function initierDropDowns() {
-    console.log("initierDropDowns");
-    // Fyll inn dropdown for våpen
-    // db.transaction(function(tx) {
-    // initierListe(tx, "VAAPEN", $("#selectVaapen"));
-    // }, DbErrorHandler);
-    initierDropDown("VAAPEN", "#selectVaapen");
+	console.log("initierDropDowns");
+	// Fyll inn dropdown for våpen
+	// db.transaction(function(tx) {
+	// initierListe(tx, "VAAPEN", $("#selectVaapen"));
+	// }, DbErrorHandler);
+	initierDropDown("VAAPEN", "#selectVaapen");
 }
 
 //  ********************************************
 //  Initier DropDown
 // 	********************************************
 function initierDropDown(tbl, side) {
-    // Fyll inn dropdown for kuler
-    db.transaction(function(tx) {
-        initierListe(tx, tbl, $(side));
-    }, DbErrorHandler);
+	// Fyll inn dropdown for kuler
+	db.transaction(function(tx) {
+		initierListe(tx, tbl, $(side));
+	}, DbErrorHandler);
 }
 
 //  ********************************************
 //  Populate the database
 // 	********************************************
 function initierDatabase(tx) {
-    console.log("initierDatabase");
-    initierTabellSikte(tx);
-    initierTabellKule(tx);
-    initierTabellVaapen(tx);
-    // initierTabellSisteBruk(tx);
+	console.log("initierDatabase");
+	initierTabellSikte(tx);
+	initierTabellKule(tx);
+	initierTabellVaapen(tx);
+	// initierTabellSisteBruk(tx);
 }
 
 //  ********************************************
 //  Transaction error callback
 //  ********************************************
 function DbErrorHandler(err) {
-    console.log("DbErrorHandler");
-    console.log("Error processing SQL: " + err.message);
+	console.log("DbErrorHandler");
+	console.log("Error processing SQL: " + err.message);
 }
 
 //  ********************************************
 //  Eventhandlers for enter/leave pages
 //  ********************************************
 $(function() {
-    $('#vaapenOppsett').on('pagebeforeshow', function() {
-        console.log("vaapenOppsett: pagebeforeshow triggered");
-        if (!isVaapenOppsettLoaded) {
-            initierDropDown("KULE", "#selectKule");
-            isVaapenOppsettLoaded = true;
-            console.log("\tloaded KULE");
-        }
-    });
-    $('#vaapenOppsett').on('pagebeforehide', function() {
-        console.log("vaapenOppsett: pagebeforehide triggered");
-        // $('#selectKule').find('option').remove().end();
-        // $('#selectKule').selectmenu('refresh');
-        // isVaapenOppsettLoaded = false;
-    });
-    //  *********************************************
-    //  Tap and Hold
-    //  *********************************************
-    $("#selectVaapen-button").bind('taphold', function(event) {
-        console.log("tapholdHandler");
-        $.mobile.changePage("#vaapenOppsett");
-    });
+	$('#vaapenOppsett').on('pagebeforeshow', function() {
+		console.log("vaapenOppsett: pagebeforeshow triggered");
+		if (!isVaapenOppsettLoaded) {
+			initierDropDown("KULE", "#selectKule");
+			isVaapenOppsettLoaded = true;
+			console.log("\tloaded KULE");
+		}
+	});
+	$('#vaapenOppsett').on('pagebeforehide', function() {
+		console.log("vaapenOppsett: pagebeforehide triggered");
+		// $('#selectKule').find('option').remove().end();
+		// $('#selectKule').selectmenu('refresh');
+		// isVaapenOppsettLoaded = false;
+		// $( "#vaapenEdit" ).css( "visibility", "visible" );
+	});
+	//  *********************************************
+	//  Tap and Hold
+	//  *********************************************
+	$("#selectVaapen-button").bind('taphold', function(event) {
+		console.log("tapholdHandler");
+		$.mobile.changePage("#vaapenOppsett", {
+			transition : "none"
+		});
+	});
 });
 
 //  ********************************************
 //  TESTING
 //  ********************************************
 function sjekkBasic() {
-    console.log("sjekkBasic");
-    //get
-    var bla = $('#basic').val();
-    console.log("Value entered: " + bla);
-    //set
-    $('#basic').val('Skriv inn ny tekst');
+	console.log("sjekkBasic");
+	//get
+	var bla = $('#basic').val();
+	console.log("Value entered: " + bla);
+	//set
+	$('#basic').val('Skriv inn ny tekst');
 }
